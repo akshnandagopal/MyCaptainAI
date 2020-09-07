7728dde196b12b5671bf7a986a47f321aac4b8a7
akshnandagopal <62838118+akshnandagopal@users.noreply.github.com>
2020-09-07 20:19:08 +0530

    deleted a file posnum.py
    I am re creating this file with the same name

--- posnum.py
+++
@@ -1,9 +0,0 @@
-list1=[12,-7,5,64,-14]
-for positive in list1:
-    if(positive % 2 == 0):
-        print(positive)
-        
-list2=[12,14,-15,3]
-for positives in list2:
-    if(positives % 2 == 0):
-        print(positives)
