b319e2b2ee30ec75184496d41a6b1ea03258bfc6
akshnandagopal <62838118+akshnandagopal@users.noreply.github.com>
2020-09-07 20:20:07 +0530

    created a file named posnum.py
    This program prints all the positive numbers in a list which is given as input.

---
+++ posnum.py
@@ -0,0 +1,13 @@
+list1=[12,-7,5,64,-14]
+pos1=[]
+for positive in list1:
+    if(positive > 0):
+        pos1.append(positive)
+print(pos1)
+        
+list2=[12,14,-15,3]
+pos2=[]
+for positives in list2:
+    if(positives > 0):
+        pos2.append(positives)
+print(pos2)
