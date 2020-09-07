4114a84a338c1979fd4cac4db0153594d7ad3324
akshnandagopal <62838118+akshnandagopal@users.noreply.github.com>
2020-09-07 20:41:41 +0530

    created a file named datastruct.py
    This program has codes for accessing,assigning and deleting elements from a tuple, in a list and from a dictionary respectively.

---
+++ datastruct.py
@@ -0,0 +1,17 @@
+#assigning elements to different lists
+lang=['C','C++']
+tech=['AI','IOT']
+lang.append("Java")
+tech.append("Blockchain")
+print(lang)
+print(tech)
+
+
+#accessing elements from a tuple
+python=('Numpy','Pandas')
+print(python[0])
+
+#deleting different dictionary elements
+webdev={'lang1':'html','lang2':'css','lang3':'javascript'}
+del webdev['lang2']
+print(webdev)
